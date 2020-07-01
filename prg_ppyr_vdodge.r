25bd399ce56ec3c04ac1728614d8c3f10c4ddf33
Applied Demography Toolbox <45884231+AppliedDemogToolbox@users.noreply.github.com>
2020-07-01 10:54:38 -0700

    Update prg_ppyr_vdodge.r

--- prg_ppyr_vdodge.r
+++ prg_ppyr_vdodge.r
@@ -18,7 +18,7 @@ library(data.table)
 library(ggthemes)
 library(forcats)
 
-bd <- fread('Casos1_2103B.csv', encoding = 'UTF-8')
+bd <- fread('https://raw.githubusercontent.com/CescRf/population-PYRAMID-WITH-A-DECOMPOSITION-FOR-THREE-VARIABLES/master/Casos1_2103B.csv', encoding = 'UTF-8')
 
 bdb <- bd %>% 
   count(Sexo, Edad, `Tipo*`) %>% 
